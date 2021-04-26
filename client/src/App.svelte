5fbfc773dee3f800cf88514060e7d32ca777ff80
Loubna Soulimani <lsoulima@e2r10p11.1337.ma>
2021-04-26 12:43:07 +0000

    Dynamic attributes

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,5 +1,7 @@
 <script>
-	export let name;
+	let alt = "img";
+	let src =
+		"https://media-exp1.licdn.com/dms/image/C561BAQFGwG_I_yubDA/company-background_10000/0/1519798679721?e=2159024400&v=beta&t=NZ_SyDW_YDs1cNdohA56e9bbg1N2MnP_co4wYZf55iY";
 </script>
 
-<h1>Hello {name}!</h1>
+<img {src} {alt} />
