420d3fa6dfb1a433af8d15c9816797afec240205
Loubna Soulimani <lsoulima@e2r10p11.1337.ma>
2021-04-26 12:45:07 +0000

    Styling

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,9 @@
-<script>
-	let alt = "img";
-	let src =
-		"https://media-exp1.licdn.com/dms/image/C561BAQFGwG_I_yubDA/company-background_10000/0/1519798679721?e=2159024400&v=beta&t=NZ_SyDW_YDs1cNdohA56e9bbg1N2MnP_co4wYZf55iY";
-</script>
+<style>
+	p {
+		color: purple;
+		font-family: "Comic Sans MS", cursive;
+		font-size: 2em;
+	}
+</style>
 
-<img {src} {alt} />
+<p>Loubna Soulimani</p>
